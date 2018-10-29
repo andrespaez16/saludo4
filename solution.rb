ab1c5535a413853262cd51b407b3a7eec64a9907
andrespaez16 <kinkbmx216@gmail.com>
2018-10-29 11:28:41 -0500

    nueva_version_full

--- solution.rb
+++ solution.rb
@@ -1,15 +1,22 @@
 require"sinatra"
 
-get "/" do
- unless params[:nombre]
-       @nombre = "desconocido"
- else    
-    @nombre = params[:nombre]   
-    end
-erb :"/saludo"
+get "/:nombre" do
+#  unless params[:nombre]
+#        @nombre = "desconocido"
+#  else    
+#     @nombre = params[:nombre]   
+#     end
+# erb :"/saludo"
+@nombre = params[:nombre]
+erb :saludo
 end
 
-get "/:nombre" do
-@nombre = params[:nombre]
-erb :"/saludo"
-end
+# get "/:nombre" do
+# @nombre = params[:nombre]
+# erb :/saludo"
+# end
+# get '/dog/:name' do
+#       p params
+#        @name =params["name"]
+#       erb :makers
+#       end
